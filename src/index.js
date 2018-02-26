7b8920301583591051f233f94fc617739c4c7977
thaseugene <thaseugene@gmail.com>
2018-02-26 10:38:20 +0200

    'solution'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  return true;
+    var arr = [];
+
+    for (var i = 0; i < bracketsConfig.length; i++) {
+        stri = String(bracketsConfig[i]).split(',');
+
+        console.log(str.charAt(str.length - 1));
+        if (~str.charAt(0).indexOf(stri[0]) && ~str.charAt(str.length - 1).indexOf(stri[1])) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
 }
